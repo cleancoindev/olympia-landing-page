680b09ec1e5441169a3eea7f2b17c6a4373490c1
Mikhail Mikheev <mdoublemmm@gmail.com>
2018-08-29 15:19:31 +0400

    Add form PM-765

--- js/form.js
+++ js/form.js
@@ -1,24 +1,56 @@
+const showError = text => {
+  const container = document.getElementsByClassName('signup-container')
+  const prevMessage = document.getElementsByClassName('error-message')[0]
+  if (prevMessage) {
+    prevMessage.parentNode.removeChild(prevMessage)
+  }
+
+  const errorMessage = document.createElement('span')
+
+  errorMessage.innerText = text
+  errorMessage.className = 'error-message'
+
+  container[0].appendChild(errorMessage)
+}
+
+const showSuccess = text => {
+  const container = document.getElementsByClassName('signup-container')
+  const prevMessage = document.getElementsByClassName('signup-success')[0]
+  if (prevMessage) {
+    prevMessage.parentNode.removeChild(prevMessage)
+  }
+
+  const successMessage = document.createElement('span')
+
+  successMessage.innerText = text
+  successMessage.className = 'signup-success'
+
+  container[0].appendChild(successMessage)
+}
+
 subscribe = _ => {
   const API_URL = 'https://us13.api.mailchimp.com/3.0/lists/a695408992/members/'
   const API_KEY = '7aa9a2934867fc0b60d9e988b91c9a55-us13'
-  const headers = new Headers()
-
-  headers.set('Authorization', 'Basic ' + Buffer.from('gnosis' + ':' + API_KEY).toString('base64'))
-
   const email_address = document.getElementById('emailAddress').value
 
   fetch(API_URL, {
     method: 'POST',
-    headers: {
-      Authorization: 
-      'Basic ' + Buffer.from('gnosis' + ':' + API_KEY).toString('base64'),
-    },
+    credentials: 'include',
+    headers: new Headers({
+      Authorization: `Basic ${Buffer.from(`apikey:${API_KEY}`).toString('base64')}`,
+    }),
     body: JSON.stringify({
       email_address,
       status: 'subscribed',
     }),
-  })
-  debugger
+  }).then(
+    () => {
+      showSuccess('Please check your email for subscription confirmation.')
+    },
+    () => {
+      showError('Something went wrong. Please try again later.')
+    },
+  )
 
   return false
 }
